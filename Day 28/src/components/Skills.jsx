8cc1e292a31b359c6a0f926412814abd819c3835
unknown <youloos477@gmail.com>
2025-05-09 16:38:35 +0500

    Okay Skills Complete

--- Day 28/src/components/Skills.jsx
+++ Day 28/src/components/Skills.jsx
@@ -7,9 +7,45 @@ const Skills = () => {
             <div className="contain">
                 <div className="part1">
                     <h1>My Skills</h1>
-                    <div class="range-box">
-                        <input type="range" min="0" max="100" value="70" disabled />
-                        <span>70%</span>
+                    <div className="Ranges">
+                        <div class="range-container">
+                            <div class="range-box">
+                                <p>HTML</p>
+                                <input type="range" min="0" max="100" value="30" disabled />
+                                <span>30%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>CSS</p>
+                                <input type="range" min="0" max="100" value="50" disabled />
+                                <span>50%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>JavaScript</p>
+                                <input type="range" min="0" max="100" value="80" disabled />
+                                <span>80%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>JavaScript</p>
+                                <input type="range" min="0" max="100" value="80" disabled />
+                                <span>80%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>JavaScript</p>
+                                <input type="range" min="0" max="100" value="80" disabled />
+                                <span>80%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>JavaScript</p>
+                                <input type="range" min="0" max="100" value="80" disabled />
+                                <span>80%</span>
+                            </div>
+                            <div class="range-box">
+                                <p>JavaScript</p>
+                                <input type="range" min="0" max="100" value="80" disabled />
+                                <span>80%</span>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
