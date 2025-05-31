e1318e3d9c24c15058cc2906a995f369e084f7df
unknown <youloos477@gmail.com>
2025-05-31 15:06:39 +0500

    Binary Complete

--- Day 20 Complete/Day 14/Binary_search.js
+++ Day 20 Complete/Day 14/Binary_search.js
@@ -2,6 +2,8 @@
 
 // ***************** Binary Search **********************
 
+// Method 1
+// دونوں valid ہیں, لیکن اگر آپ professional یا reusable code لکھنا چاہتے ہیں تو Method 1 بہتر ہے۔
 const binar = (num, targ) => {
     let start = 0;
     let end = num.length - 1;
@@ -18,3 +20,31 @@ const binar = (num, targ) => {
     return -1;
 }
 console.log(binar([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14], 12));
+
+
+// Method 2 
+
+// اگر آپ beginner ہیں تو آپ کو Method 2 استعمال کرنا چاہیے۔
+// ✅ کیوں؟
+// Method 2 میں steps واضح ہیں:
+// array الگ define کیا گیا ہے
+// function کو parameters کے ساتھ call کیا گیا ہے
+// logic کو سمجھنا آسان ہے کیونکہ ہر step الگ ہے
+
+
+
+let arr = [2, 5, 8, 9, 12, 18, 20, 25, 67, 101, 125];
+let indx = binarySearch(arr, 0, arr.length - 1, 101);
+if (indx === -1) console.log("not found");
+else console.log("Target found at", indx , "index");
+
+
+function binarySearch(arr, first, last, target) {
+    while (first <= last) {
+        let mid = Math.floor((first + last) / 2);
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] > target) last = mid - 1;
+        else first = mid + 1
+    }
+    return -1;
+}
