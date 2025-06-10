bd43f8aa5e42b5498b064f6de6ffb39d7c772347
unknown <youloos477@gmail.com>
2025-06-10 12:47:07 +0500

    Day 45 Start

---
+++ Day 50 Complete/Day 45/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Context from './component/Context'
+
+function App() {
+
+  return (
+    <>
+      <Context />
+    </>
+  )
+}
+
+export default App;
+
+
+// https://chatgpt.com/c/6847afb2-043c-8011-96a6-783f519aa9e6
---
+++ Day 50 Complete/Day 45/src/component/Context.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Context = () => {
+  return (
+    <>
+        <h1>Context API</h1>
+    </>
+  )
+}
+
+export default Context
