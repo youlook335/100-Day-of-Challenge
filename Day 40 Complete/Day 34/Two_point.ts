85386ff7935d48252e408bb872e2dbd7250d1ac4
unknown <youloos477@gmail.com>
2025-06-14 14:13:38 +0500

    Start A Learning TypeScript

---
+++ Day 40 Complete/Day 34/Two_point.ts
@@ -0,0 +1,2 @@
+// https://www.youtube.com/watch?v=IEgp1aRqQ7w
+// https://chatgpt.com/c/684cfb1c-f4fc-8011-9165-d5a310551c0e
---
+++ Learning a Typescript/Data_Type.ts
@@ -0,0 +1,1 @@
+// https://chatgpt.com/c/684cfdc6-75a0-8011-9a0e-301de4f7006a
