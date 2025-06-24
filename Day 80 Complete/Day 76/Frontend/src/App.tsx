e9b69486dfb9b5113e69a33b24cbd8892486da2d
unknown <youloos477@gmail.com>
2025-06-24 19:44:49 +0500

    Add placeholder text to App component
    Inserted a sample paragraph with placeholder text into the App component for display purposes.

--- Day 80 Complete/Day 76/Frontend/src/App.tsx
+++ Day 80 Complete/Day 76/Frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-     
+     <p>This is a Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptates distinctio ratione rerum maiores amet natus exercitationem consequuntur repellendus veritatis incidunt maxime, mollitia, dolorum sed quos praesentium laborum. Distinctio, iste?</p>
     </>
   )
 }
